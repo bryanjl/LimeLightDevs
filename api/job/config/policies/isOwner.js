a9f7f924adc5c613d067bd465931c8fb596ba98f
Bryan Lilly <bryanjameslilly@gmail.com>
2021-10-03 20:21:26 +0700

    Started testing routes - changes to job controller due to bugs

--- api/job/config/policies/isOwner.js
+++ api/job/config/policies/isOwner.js
@@ -1,11 +1,11 @@
 module.exports = async(ctx, next) => {
-    let job = await strapi.services.jobs.findOne({ id: ctx.params.id });
+    let job = await strapi.services.job.findOne({ id: ctx.params.id });
 
     if(!job){
         return ctx.response.notFound(`Job posting with id of ${ctx.params.id} not found`);
     }
 
-    if(job.users_permissions_user.email === ctx.state.user.email || ctx.state.user.organization_role === 'Organization_Admin'){
+    if(job.users_permissions_user.email === ctx.state.user.email){
         return await next();
     } else {
         return ctx.unauthorized(`You are not the owner of this job posting`);
