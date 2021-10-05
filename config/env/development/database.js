73d0d879988cb3d6ad197d39b1be9e2fcbcda463
Bryan Lilly <bryanjameslilly@gmail.com>
2021-10-05 09:06:51 +0700

    fixed organization create function - updated DB config)

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,17 +4,13 @@ module.exports = ({ env }) => ({
       default: {
         connector: 'mongoose',
         settings: {
-          host: env('DATABASE_HOST', 'limelightdevs.5wq7a.mongodb.net'),
-          srv: env.bool('DATABASE_SRV', true),
-          port: env.int('DATABASE_PORT', 27017),
-          database: env('DATABASE_NAME', 'LimeLightDevs'),
-          username: env('DATABASE_USERNAME', 'limelightdevs'),
-          password: env('DATABASE_PASSWORD', 'limelightdevs'),
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          username: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
         },
-        options: {
-          authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-          ssl: env.bool('DATABASE_SSL', true),
-        },
+        options: {},
       },
     },
   });
